3ff926756bc650b51281f826e816615b0440f50b
EfrainDs3 <santiagotorresmurrieta@gmail.com>
2025-09-07 16:34:13 -0500

    subiendo mi proyecto completo

---
+++ detalles/src/main/java/fisi/software/detalles/DetallesApplication.java
@@ -0,0 +1,13 @@
+package fisi.software.detalles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetallesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetallesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fisi/software/detalles/DetallesApplicationTests.java
@@ -0,0 +1,13 @@
+package fisi.software.detalles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetallesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
