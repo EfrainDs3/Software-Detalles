0de5d55bf3f994fd0a389c5d4addea37e574938d
EfrainDs3 <santiagotorresmurrieta@gmail.com>
2025-09-07 15:44:50 -0500

    mi primer commit

---
+++ src/main/java/detalles/com/fisi/FisiApplication.java
@@ -0,0 +1,13 @@
+package detalles.com.fisi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FisiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/detalles/com/fisi/FisiApplicationTests.java
@@ -0,0 +1,13 @@
+package detalles.com.fisi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
